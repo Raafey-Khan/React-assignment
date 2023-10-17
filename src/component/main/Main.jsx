c940fdc7f96dd8f29433e17e521819e5f5cd961d
Raafey khan <113880768+Raafey-Khan@users.noreply.github.com>
2023-10-17 12:53:51 +0530

    inital commit

---
+++ src/component/main/Main.jsx
@@ -0,0 +1,285 @@
+import React from 'react';
+import "./main.css";
+
+export default function Main() {
+  return (
+    <div>
+        <head>
+  <meta charset="utf-8" />
+  <link rel="icon" href="/favicon.ico" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#000000" />
+  <title>Michael Jackson</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins%3A400%2C700"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400%2C700"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans%3A400%2C700"/>
+  <link rel="stylesheet" href="./styles/michael-jackson.css"/>
+</head>
+<body>
+<div class="michael-jackson-TNG">
+  <div class="auto-group-7k7j-vFr">
+    <div class="group-35375-7bE">
+      <div class="group-35369-A3i">
+        <div class="rectangle-437-GMe">
+        </div>
+        <div class="ellipse-1-xEU">
+        </div>
+        <div class="ellipse-2-rak">
+        </div>
+        <div class="ellipse-3-mBv">
+        </div>
+      </div>
+    </div>
+    <p class="about-michael-jackson-DJp">About Michael Jackson</p>
+    <div class="auto-group-yjsa-ShN">
+      <div class="group-35376-vcY">
+        <p class="from-T6g">FROM</p>
+        <p class="india-Ytp">INDIA</p>
+      </div>
+      <div class="group-35377-3ag">
+        <p class="partner-since-AvC">PARTNER SINCE</p>
+        <p class="item-2011-GyE">2011</p>
+      </div>
+      <div class="group-35378-aU8">
+        <p class="average-response-time-Wsa">AVERAGE RESPONSE TIME</p>
+        <p class="minutes-Qi4">30 minutes</p>
+      </div>
+    </div>
+    <div class="group-35379-2jS">
+      <p class="about-jNx">ABOUT</p>
+      <p class="i-am-a-professional-charted-accountant-here-to-offer-professional-services-of-accounting-and-finance-financial-statements-bookkeeping-in-affordable-price-qB6">I am a Professional Charted Accountant here to offer professional services of accounting and finance, financial statements, Bookkeeping in affordable price.</p>
+    </div>
+    <div class="auto-group-5ymv-3HA">
+      <div class="auto-group-d5wx-WRe">
+        <p class="services-i-offer-2et">SERVICES I OFFER</p>
+        <p class="financial-accounting-financial-statements-bookkeeping-accounting-and-finance-corporate-finance-maintain-charts-of-accounts-profit-and-loss-statements-bank-reconciliation-balance-sheets-vEU">
+        Financial accounting
+        <br/>
+        Financial statements
+        <br/>
+        Bookkeeping
+        <br/>
+        Accounting and finance
+        <br/>
+        Corporate Finance
+        <br/>
+        Maintain Charts of Accounts
+        <br/>
+        Profit and loss statements
+        <br/>
+        Bank Reconciliation
+        <br/>
+        Balance Sheets
+        </p>
+      </div>
+      <div class="group-35380-WiU">
+        <p class="why-me-3TW">WHY ME?</p>
+        <p class="one-time-delivery-24-7-customer-support-error-free-documents-kMv">
+        One-time delivery
+        <br/>
+        24/7 customer support
+        <br/>
+        Error-free documents
+        </p>
+      </div>
+    </div>
+    <div class="group-35382-QBa">
+      <p class="recommended-for-you-Lqv">Recommended for you</p>
+      <div class="group-35381-eLp">
+        <div class="auto-group-nrma-Mm2">
+          <img class="rectangle-376-Gsz" src="./assets/rectangle-376.png"/>
+          <div class="auto-group-34jl-y1i">
+            <div class="auto-group-uxzj-3GU">
+              <p class="michael-jackson-LFa">Michael Jackson</p>
+              <p class="item-4370-2u6">₹4,370</p>
+            </div>
+            <p class="i-will-do-business-evaluation-and-corporate-services-iG8">I will do business evaluation and corporate services</p>
+            <div class="group-35357-kic">
+              <img class="star-fill-ogt" src="./assets/star-fill.png"/>
+              <p class="item-48-89-i3A">
+                <span class="item-48-89-i3A-sub-0">4.8 </span>
+                <span class="item-48-89-i3A-sub-1">(89)</span>
+              </p>
+            </div>
+            <div class="component-1-ecx">View services</div>
+          </div>
+        </div>
+        <div class="auto-group-lszn-ZtL">
+          <img class="rectangle-376-5bn" src="./assets/rectangle-376-PC4.png"/>
+          <div class="auto-group-bvy4-Nat">
+            <div class="auto-group-jaet-qjN">
+              <p class="stevie-wonder-XcC">Stevie Wonder</p>
+              <p class="item-4263-pbJ">₹4,263</p>
+            </div>
+            <p class="i-will-do-business-evaluation-and-corporate-services-WDE">I will do business evaluation and corporate services</p>
+            <div class="group-35357-8kQ">
+              <img class="star-fill-bP6" src="./assets/star-fill-z3A.png"/>
+              <p class="item-50-62-fdr">
+                <span class="item-50-62-fdr-sub-0">5.0 </span>
+                <span class="item-50-62-fdr-sub-1">(62)</span>
+              </p>
+            </div>
+            <div class="component-1-kak">View services</div>
+          </div>
+        </div>
+        <div class="auto-group-lvag-HUC">
+          <img class="rectangle-376-BpU" src="./assets/rectangle-376-Wak.png"/>
+          <div class="auto-group-ilwz-UYg">
+            <div class="auto-group-pqos-ZKE">
+              <p class="ray-charles-FSx">Ray Charles</p>
+              <p class="item-2586-kec">₹2,586</p>
+            </div>
+            <p class="i-will-do-business-evaluation-and-corporate-services-SGY">I will do business evaluation and corporate services</p>
+            <div class="group-35357-sse">
+              <img class="star-fill-YD6" src="./assets/star-fill-E8x.png"/>
+              <p class="item-43-189-39r">
+                <span class="item-43-189-39r-sub-0">4.3 </span>
+                <span class="item-43-189-39r-sub-1">(189)</span>
+              </p>
+            </div>
+            <div class="component-1-ahW">View services</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="top-bar-4m6">
+    <div class="image-1-J9e">
+    </div>
+    <div class="frame-34631-HXN">
+      <p class="solutions-aFa">Solutions</p>
+      <img class="down-arrow-TqA" src="./assets/down-arrow-VQU.png"/>
+    </div>
+    <div class="frame-34632-NhE">
+      <p class="features-Tig">Features</p>
+      <img class="down-arrow-NKr" src="./assets/down-arrow-KUt.png"/>
+    </div>
+    <p class="text-Utg">Blogs</p>
+    <div class="frame-34630-bTW">
+      <p class="about-gjr">About</p>
+      <img class="down-arrow-BwW" src="assets/down-arrow-iSU.png"/>
+    </div>
+    <div class="sign-up-button-JWL">Login</div>
+    <div class="sign-up-button-Vap">Register</div>
+  </div>
+  <div class="footer-iiU">
+    <div class="footer-cor">
+      <div class="auto-group-hqzn-Jwa">
+        <div class="auto-group-u7uc-bfn">
+          <div class="image-1-7u2">
+          </div>
+          <p class="indias-first-platform-dedicated-to-simplifying-partner-search-xek">India&#39;s first platform dedicated to simplifying partner search</p>
+        </div>
+        <div class="group-35020-ovG">
+          <div class="company-UmW">COMPANY</div>
+          <div class="auto-group-fmyz-Pda">
+            <div class="text-tqE">About </div>
+            <div class="pricing-1ex">Pricing</div>
+            <div class="careers-7xt">Careers</div>
+          </div>
+        </div>
+        <div class="group-35310-dRS">
+          <div class="solutions-WVE">SOLUTIONS</div>
+          <div class="auto-group-fazc-2Cg">
+            <div class="text-j76">Search</div>
+            <div class="text-3da">Connect</div>
+            <div class="text-9wW">Research</div>
+            <div class="text-ssW">Academy</div>
+          </div>
+        </div>
+        <div class="group-35018-nja">
+          <div class="resources-ga4">RESOURCES</div>
+          <div class="auto-group-znqe-BWp">
+            <div class="text-hVA">Blogs</div>
+            <div class="text-NrC">Forms</div>
+          </div>
+        </div>
+        <div class="group-35017-61W">
+          <div class="auto-group-qfu2-jqA">
+            <div class="support-ex8">SUPPORT</div>
+            <div class="text-LKA">Help</div>
+          </div>
+          <div class="text-3UU">Contact Us</div>
+        </div>
+        <div class="group-35015-kdn">
+          <div class="auto-group-49zj-FqS">
+            <div class="legal-MtU">LEGAL</div>
+            <div class="text-Sf2">Privacy</div>
+            <div class="text-LEc">Terms</div>
+          </div>
+          <div class="text-qx4">Accessibility</div>
+        </div>
+      </div>
+      <div class="auto-group-fexz-9Sx">
+        <div class="f-Qdn">f</div>
+        <div class="f-t3A">f</div>
+      </div>
+      <img class="vector-195-mMr" src="REPLACE_IMAGE:2154:177"/>
+      <div class="auto-group-jpku-EWL">
+        <p class="sambhaji-nagar-no2-st-antony-road-chembur-mumbai-400071-maharashtra-85v">Sambhaji Nagar, No2, St. Antony Road, Chembur, Mumbai - 400071, Maharashtra</p>
+        <div class="group-34830-yMS">
+          <img class="footers-social-link-J8p" src="./assets/footers-social-link-6UG.png"/>
+          <img class="footers-social-link-nZn" src="./assets/footers-social-link-B4L.png"/>
+          <div class="auto-group-ihma-Vj6">
+            <div class="linkedin-logo-1xL">
+              <img class="vector-5hJ" src="./assets/vector-LMe.png"/>
+              <div class="f-BkL">f</div>
+            </div>
+            <img class="footers-social-link-d6Y" src="./assets/footers-social-link-8gg.png"/>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="copyright-footer-4he">Registered trademark of India Private Limited © 2023 loerumipsum Inc. All Rights Reserved</div>
+  </div>
+  <div class="frame-34633-Jrt">
+    <div class="auto-group-svf6-aZW">
+      <p class="michael-jackson-h8L">Michael Jackson</p>
+      <p class="i-am-here-to-provide-my-expertise-in-accounting-and-finance-which-includes-financial-statements-economics-and-auditing-all-to-assist-you-effectively-zdE">
+      I am here to provide my expertise in accounting and finance, which includes financial statements, economics, and auditing, all to assist 
+      <br/>
+      you effectively
+      </p>
+      <div class="group-35357-Ndn">
+        <img class="star-fill-4mW" src="./assets/star-fill-roz.png"/>
+        <p class="item-48-89-k8Y">
+          <span class="item-48-89-k8Y-sub-0">4.8 </span>
+          <span class="item-48-89-k8Y-sub-1">(89)</span>
+        </p>
+      </div>
+    </div>
+    <div class="auto-group-db1j-zac">
+      <div class="auto-group-2jgq-44g">
+        <p class="basic-to-complex-tasks-yhS">Basic to complex tasks</p>
+        <p class="item-4370-UPJ">₹4,370</p>
+      </div>
+      <div class="group-35370-xJU">
+        <img class="calendar-2-line-ctp" src="./assets/calendar-2-line.png"/>
+        <p class="delivers-the-job-within-2-days-Hk4">Delivers the job within 2 days</p>
+      </div>
+      <div class="auto-group-s49n-nB2">
+        <div class="component-1-tUx">Request Proposal</div>
+        <div class="component-7-VUk">Chat with me</div>
+      </div>
+    </div>
+    <div class="group-35373-bw6">
+      <p class="what-people-say-UEC">What people say?</p>
+      <p class="i-cannot-express-enough-gratitude-for-the-support-micheal-has-provided-in-managing-my-personal-finances-their-attention-to-detail-and-deep-understanding-of-financial-markets-has-ensured-that-my-investments-are-in-safe-hands-i-highly-recommend-their-services-to-anyone-seeking-financial-guidance-YE4">I cannot express enough gratitude for the support Micheal has provided in managing my personal finances. Their attention to detail and deep understanding of financial markets has ensured that my investments are in safe hands. I highly recommend their services to anyone seeking financial guidance.</p>
+      <div class="group-35370-TVS">
+        <div class="rectangle-437-WyW">
+        </div>
+        <div class="ellipse-1-R4t">
+        </div>
+        <div class="ellipse-2-jLU">
+        </div>
+        <div class="ellipse-3-eTS">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+         
+    </div>
+  )
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./home.css"
+import Main from '../../component/main/Main'
+
+export default function Home() {
+  return (
+    <>
+    <div>
+        <Main/>
+    </div>
+    </>
+  )
+}
